26bd67cbd1b9237fa083478d611716bb1abd0477
13bcs2055 <nandhini.13bcs2055@mkce.ac.in>
2016-08-28 10:59:38 +0530

    Update and rename even.java to evenno.java

---
+++ evenno.java
@@ -0,0 +1,23 @@
+package js;
+import java.util.Scanner;
+public class evenno {
+	
+	
+	
+	    public static void main (String[] args) 
+	    {
+	        int n;
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Enter the number you want to check:");
+	        n = s.nextInt();
+	        if(n == 0)
+	        {
+	            System.out.println("The given number "+n+" is even");
+	        }
+	        else
+	        {
+	            System.out.println("The given number "+n+" is odd");
+	        }
+	        
+	    }
+	}
